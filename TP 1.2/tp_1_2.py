dfe9a2395a3647c942045f86d34b16ebbdffd855
Nicolás Fierro <nico.fierro1@gmail.com>
2022-03-29 22:44:06 -0300

    Graficas

--- TP 1.2/tp_1_2.py
+++ TP 1.2/tp_1_2.py
@@ -2,50 +2,64 @@ from clases import negros, rojos, randint, apuesta_minima, JugadorMG, JugadorPar
 import matplotlib as plt
 
 
+def graficoDineroUnicaTirada(dineroTiempo, titulo,dineroInicial):
+    plt.title(titulo)
+    plt.axhline(dineroInicial, color='k', ls="solid")
+    plt.plot(dineroTiempo[0], linewidth=1.2)
+    plt.xlabel("(Número de tiradas)")
+    plt.ylabel("Capital en el tiempo")
+    # Me ajustan los x e y
+    ax = plt.gca()
+    ax.relim()
+    ax.autoscale_view()
+    plt.axhline(dineroInicial,color='w', ls="solid",visible=False) #Linea invisible para agregar legend
+    dineroIni = 'Capital inicial: ' + str(dineroInicial)
+    dineroFin=('Capital Final: ' + str(dineroTiempo[0][t-1]))
+    plt.legend((dineroIni,dineroFin), loc="best")
+    plt.ioff()
+    plt.show()
+
+
+def graficoDineroTiradasMultiples(dineroTiempo, titulo,dineroInicial):
+    for i in range(c):
+        plt.title(titulo)
+        plt.axhline(dineroInicial, color='k', ls="solid")
+        plt.axhline(dineroInicial * 1.5, color='c', ls="-", linewidth=0.8)
+        plt.axhline(dineroInicial * 0.5, color='r', ls="-")
+        plt.plot(dineroTiempo[i], linewidth=1.3)
+        plt.xlabel("(Número de tiradas)")
+        plt.ylabel("Capital en el tiempo")
+        # Me ajustan los x e y
+        ax = plt.gca()
+        ax.relim()
+        ax.autoscale_view()
+        plt.axhline(dineroInicial,color='w', ls="solid",visible=False) #Linea invisible para agregar legend
+        dineroIni = 'Capital inicial: ' + str(dineroInicial)
+        cincuentamas =('Ganancia del 50%')
+        cincuentamenos = ('Pérdida del 50%:')
+        plt.legend((dineroIni,cincuentamas,cincuentamenos), loc="best")
+        plt.ioff()
+    plt.show()
+
+def graficaFrecFavorable(frecuencias, title):
+    plt.title(title)
+    plt.bar(range(0,len(frecuencias)),frecuencias)
+    plt.ylabel('Frec. Relativa de apuesta favorable')
+    plt.ylim(0, 1)
+    plt.xlabel('n (número de tiradas)')
+    plt.show()
+
+
 def tirada() -> int:
     return randint(0, 36)
 
 
-apuesta_minima = 1  # float(input("Ingresar monto minimo de apuesta: "))
 
-monto_inicial = 100  # float(input("Ingresar monto inicial de cada jugador: "))
 
-martingala = []
-paroli = []
-guetting = []
+# dineroMartinGalaAcotado= [[0 for x in range(t)] for y in range(c)]
+# frecRelativasMG = []
+# dineroMartinGalaAcotadM= [[0 for x in range(t)] for y in range(c)]
 
-cant_jugadores = 2  # por tipo de jugador
 
-for i in range(cant_jugadores):
-    martingala.append(JugadorMG(capital=monto_inicial,
-                      apuesta_ini=apuesta_minima))
-    paroli.append(JugadorParoli(
-        capital=monto_inicial, apuesta_ini=apuesta_minima))
-    guetting.append(JugadorGuetting(
-        capital=monto_inicial, apuesta_ini=apuesta_minima))
-
-juegos = 0
-while True:
-    juegos += 1
-    numero = tirada()
-    print(f'Tirada{juegos}---> Sale {numero}')
-    for i in range(cant_jugadores):
-        j1: JugadorMG = martingala[i]
-        j2: JugadorParoli = paroli[i]
-        j3: JugadorGuetting = guetting[i]
-        print(
-            f"{juegos}-1-1: Jugador Nº{j1.id}-->Monto prejuego={j1.capital}|| apuesta={j1.prox_apuesta}", end='')
-        j1.jugar(numero)
-        print(f"-->Monto posjuego={j1.capital}")
-        print(
-            f"{juegos}-1-2: Jugador Nº{j2.id}-->Monto prejuego={j2.capital}||", end=' ')
-        j2.jugar(numero)
-        print(f"apuesta={j2.prox_apuesta}-->Monto posjuego={j2.capital}")
-        print(
-            f"{juegos}-1-3: Jugador Nº{j3.id}-->Monto prejuego={j3.capital}||", end=' ')
-        j3.jugar(numero)
-        print(f"apuesta={j3.prox_apuesta}-->Monto posjuego={j3.capital}")
-
-    print("\n\n\n")
-    if int(input("Continuar *0* :")) == 0:
-        break
+# graficoDineroUnicaTirada(dineroMartinGalaIdeal[0],'Estrategia Martingala',capIdeal)
+# graficoDineroTiradasMultiples(dineroMartinGalaIdeal[0],'MartinGala - Capital Ideal',capIdeal)
