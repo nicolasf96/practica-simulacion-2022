3cb4e9d945ca35fba6bc75a85184b92631eca3b4
JulianSandoval25 <JulianSandoval1996@gmail.com>
2022-04-13 12:58:15 -0300

    graficasytcl

--- TP 1.1/tp1.1.py
+++ TP 1.1/tp1.1.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 plt.style.use('Solarize_Light2')
 
 #TP 1.1 Simulación de Una Ruleta
@@ -18,15 +19,17 @@ esperanza= np.arange(0, 37).mean()  #Media aritmética
 desvio= np.arange(0, 37).std() #Desviación Estándar
 varianza= np.arange(0, 37).var() #Varianza
 
-t = 30 #número de tiradas
-c = 1 #número de corridas
-nroEvaluar = 5 #np.random.randint(0,37)
+t = 10000 #número de tiradas
+c = 10 #número de corridas
+nroEvaluar = 25 #np.random.randint(0,37)
 
 
 frecuencias= [[0 for x in range(t)] for y in range(c)]
+frecuenciasAbs= [[0 for x in range(t)] for y in range(c)]
 medias= [[0 for x in range(t)] for y in range(c)]
 desvios= [[0 for x in range(t)] for y in range(c)]
 varianzas= [[0 for x in range(t)] for y in range(c)]
+
 
 
 
@@ -36,16 +39,22 @@ for i in range(0, c):
     for n in range(0, t):
         lista= numeros[:n+1]
         frecuencias[i][n]= frecuenciaR(nroEvaluar, lista)
+        frecuenciasAbs[i][n]=numeros[n]
         medias[i][n]= lista.mean()
         desvios[i][n]= lista.std()
         varianzas[i][n]= lista.var()
+
+
+frecuenciasTCL= np.sum(frecuenciasAbs, axis=0)/c
+frecuenciasTCL2=np.sum(frecuenciasAbs, axis=0)
+
 
 
 # Graficos
 fig, axs = plt.subplots(ncols=2, nrows=2, constrained_layout=True, figsize=[9, 6])
 
 
-axs[0, 0].set_title('Frecuencia Relativa (fr)')
+axs[0, 0].set_title('Frecuencia Relativa (fr) respecto a 25')
 axs[0, 0].set(xlabel='Tiradas', ylabel='Frecuencia Relativa (fr)')
 axs[0, 0].set_ylim(bottom=0, top=max(np.amax(frecuencias), frecuencia)+0.05)
 for i in range(0, c):
@@ -54,53 +63,52 @@ axs[0, 0].axhline(y=frecuencia, color='b', linestyle='-', label='fre: '+str(roun
 
 axs[0, 1].set_title('Valor Promedio (vp)')
 axs[0, 1].set(xlabel='Tiradas', ylabel='Valor Promedio (vp)')
-axs[0, 1].set_ylim(bottom=0, top=max(np.amax(medias), esperanza)+1)
 for i in range(0, c):
     axs[0, 1].plot(range(1, t+1), medias[i])#, label='corrida '+str(i+1)+'°')
 axs[0, 1].axhline(y=esperanza, color='b', linestyle='-', label='vpe: '+str(round(esperanza,3)))
 
+
 axs[1, 0].set_title('Valor del Desvío (vd)')
 axs[1, 0].set(xlabel='Tiradas', ylabel='Valor del Desvío (vd)')
-axs[1, 0].set_ylim(bottom=0, top=max(np.amax(desvios), desvio)+1)
 for i in range(0, c):
     axs[1, 0].plot(range(1, t+1), desvios[i])#, label='corrida '+str(i+1)+'°')
 axs[1, 0].axhline(y=desvio, color='b', linestyle='-', label='vde: '+str(round(desvio,3)))
 
 axs[1, 1].set_title('Valor de la Varianza (vve)')
 axs[1, 1].set(xlabel='Tiradas', ylabel='Valor de la Varianza (vv)')
-axs[1, 1].set_ylim(bottom=0, top=max(np.amax(varianzas), varianza)+10)
 for i in range(0, c):
     axs[1, 1].plot(range(1, t+1), varianzas[i])#, label='corrida '+str(i+1)+'°')
 axs[1, 1].axhline(y=varianza, color='b', linestyle='-', label='vve: '+str(round(varianza,3)))
 
 for ax in axs.flat:
     ax.legend()
-    ax.set_xlim(left=0, right=t)
+    ax.set_xlim(left=1, right=t)
 
 
 plt.show()
 
-"""def graficoProm(resultados, capAcotado):
-    plt.title("Distribucion de medias muestrales")
-    plt.axhline(capAcotado, color='k', ls="solid")
-    plt.plot(resultados, linewidth=1.2)
-    plt.xlabel("")
-    plt.ylabel("")
-    # Me ajustan los x e y
-    ax = plt.gca()
-    ax.relim()
-    ax.autoscale_view()
-    # Linea invisible para agregar legend
-    plt.axhline(50, color='w', ls="solid", visible=False)
-    #dineroIni = 'Capital inicial: ' + str(capAcotado)
-    #dineroFin = (f'Capital Final{resultados[-1]}')
-    #plt.legend((dineroIni, dineroFin), loc="best")
-    plt.ioff()
-    plt.show()
+#intervalo=round(math.sqrt(t))
+for i in range(0, c):
+    plt.hist(frecuenciasAbs[i], bins=37, edgecolor='black')
+plt.xlabel('Numeros')
+plt.ylabel('Frecuencia Absoluta')
+plt.title('Histograma de Frecuencia Absoluta')
+plt.xlim(0, 36)
+plt.show()
 
-prom_med=[]
-listas_medias=[]
-for i in medias:
-    listas_medias.append(i[-1])
 
-graficoProm(resultados=prom_med, capAcotado=0)"""
+intervalo=round(math.sqrt(len(frecuenciasTCL)))
+plt.hist(frecuenciasTCL, bins= intervalo, edgecolor='black')
+plt.xlabel('Numeros')
+plt.ylabel('Frecuencia Absoluta')
+plt.title(f'Histograma del TCL de {t} tiradas y {c} corridas ')
+plt.axline((frecuenciasTCL.mean(), 0), (frecuenciasTCL.mean(), 1), linewidth=1, color='r', linestyle='--')
+plt.show()
+
+intervalo=round(math.sqrt(len(frecuenciasTCL2)))
+plt.hist(frecuenciasTCL2, bins= intervalo, edgecolor='black')
+plt.xlabel('Numeros')
+plt.ylabel('Frecuencia Absoluta')
+plt.title(f'Histograma del TCL de {t} tiradas y {c} corridas ')
+plt.axline((frecuenciasTCL2.mean(), 0), (frecuenciasTCL2.mean(), 1), linewidth=1, color='r', linestyle='--')
+plt.show()
