05e0c26964e8228a62cf0b568e5af11fca4e423e
Ivan <ivanbisc12@gmail.com>
2022-05-19 22:16:24 -0300

    .2

--- GenDistribuido/MTransformadas.py
+++ GenDistribuido/MTransformadas.py
@@ -1,13 +1,24 @@
 from typing import Any
+from math import sqrt, log
 
 
-def Normal() -> Any:
-    pass
+def Exponencial(pseudo: list, lambda_e: float) -> Any:
+    expo = []
+    for r in pseudo:
+        x = log(1-r)/lambda_e
+        expo.append(x)
+    return expo
 
 
-def Exponencial() -> Any:
-    pass
-
-
-def Gamma() -> Any:
-    pass
+def Uniforme(pseudo: list, a: float, b: float) -> list:
+    """pseudo: Lista de numeros pseudoaleatorios
+    a:valor minimo
+    b:valor maximo
+    returns: Lista distribuida uniformemente en [a,b]"""
+    # la func de densidad es 1/b-a
+    # la func de acumulacion es x-a/b-a
+    uni = []
+    for r in pseudo:
+        x = a+(b-a)*r
+        uni.append(x)
+    return uni
