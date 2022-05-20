54a00c1d05489dd47230a481a1870c2fc92775f6
Ivan <ivanbisc12@gmail.com>
2022-05-20 16:09:49 -0300

    generador normal rechazo
    funciona

--- GenDistribuido/MRechazo.py
+++ GenDistribuido/MRechazo.py
@@ -1,13 +1,52 @@
+from operator import le
 from scipy.stats import norm
 from math import sqrt, pi, exp
 
-
-def max_densidad_norm(mu_: float, var_: float) -> tuple:
-    """Devuelve el maximo y minimo valor que puede tener f de densidad en [0,1]"""
-    f_0 = (1/var_*sqrt(2*pi))*exp(-((mu_**2)/2*(var_**2)))
-    f_1 = (1/var_*sqrt(2*pi))*exp(-(((1-mu_)**2)/2*(var_**2)))
-    return max(f_0, f_1), min(f_0, f_1)
+import matplotlib.pyplot as plt
+import numpy as np
+from MTransformadas import Uniforme, Exponencial
 
 
-def Normal(generador, mu: float, var: float, K: int) -> list:
-    pass
+def densidad_norm(m, v, x):
+    return (1/v*sqrt(2*pi))*exp(-(((x-m)**2)/2*(v**2)))
+
+
+def max_densidad_norm(mu_: float, var_: float) -> float:
+    """Devuelve el maximo valor que puede tener f de densidad en [0,1]"""
+    f_0 = densidad_norm(mu_, var_, 0)
+    f_1 = densidad_norm(mu_, var_, 1)
+    return max(f_0, f_1)
+
+
+def normal(pseudo: list, mu: float, var: float, a: float, b: float) -> list:
+    M = 1/(var*sqrt(2*pi))  # maximo valor de densidad de la funcion objetivo
+    aceptados = []
+    for U in pseudo:
+        while True:
+            V = np.random.uniform(a, b)
+            T = a+((b-a)*V)
+            if(M*U <= densidad_norm(mu, var, T)):
+                # T se acepta
+                aceptados.append(T)
+                break
+    norm = [a+((b-a)*T) for T in aceptados]
+    return norm
+
+
+def generador_numpy(n):
+    numbers = []
+    for i in range(n):
+        numbers.append(np.random.uniform(0, 1))
+    return numbers
+
+
+mu = 1
+var = 1.2
+distribucion_generada = generador_numpy(8000)
+distribucion_generada = normal(
+    distribucion_generada, mu, var, mu-(2*var), mu+(2*var))
+plt.hist(distribucion_generada)
+plt.title('Histograma de una variable')
+plt.xlabel('Valor de la variable')
+plt.ylabel('Conteo')
+plt.show()
