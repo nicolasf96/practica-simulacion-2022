73e61592c197a4f6c0c3a370aae4312398ea4aab
Ivan <ivanbisc12@gmail.com>
2022-05-19 22:16:09 -0300

    generadores distribuidos

---
+++ GenDistribuido/MRechazo.py
@@ -0,0 +1,5 @@
+from scipy.stats import norm
+
+
+def Normal(pseudo: list, mu: float, var: float) -> list:
+    pass
---
+++ Generators/__init__.py
@@ -0,0 +1,2 @@
+"""Contiene: Generadores de numeros pseudoaleatorios, tests de aleatoriedad 
+Correspondiente al tp 2.1"""
