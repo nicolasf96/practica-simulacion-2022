1580f76df8cef9d16d3b06055074118243d0ccf1
Ivan <ivanbisc12@gmail.com>
2022-05-21 19:36:31 -0300

    Uniforme con metodo de rechazo

--- GenDistribuido/MRechazo.py
+++ GenDistribuido/MRechazo.py
@@ -1,10 +1,11 @@
+from tracemalloc import start, stop
 import numpy as np
 
-from math import sqrt, pi, exp
+from math import sqrt, pi, exp, gamma
 
-import matplotlib.pyplot as plt
-import numpy as np
-from MTransformadas import Uniforme, Exponencial, Hipergeometricas, Normal
+
+"""import matplotlib.pyplot as plt"""
+"""from MTransformadas import Uniforme, Exponencial, Hipergeometricas, Normal"""
 
 
 def densidad_norm(m, v, x):
@@ -18,8 +19,13 @@ def max_densidad_norm(mu_: float, var_: float) -> float:
     return max(f_0, f_1)
 
 
-def normal(pseudo: list, mu: float, des: float) -> list:
-    a, b = mu-(5*des), mu+(5*des)
+def NormalR(pseudo: list, mu: float, des: float) -> list:
+    """Aproximacion a una distribucion N~(mu,des) con metodo
+    de aceptacion y rechazo de Von Neumann
+    pseudo: Elementos pseudoaleatorios con distribucion uniforme
+    mu:media de la funcion objetivo
+    des:desvio de la funcion objetivo"""
+    a, b = mu-(100*des), mu+(100*des)
     M = 1/(des*sqrt(2*pi))  # maximo valor de densidad de la funcion objetivo
     aceptados = []
     for U in pseudo:
@@ -34,6 +40,29 @@ def normal(pseudo: list, mu: float, des: float) -> list:
     return norm
 
 
+def densidad_gamma(lmbda: float, alpha: float, x: float) -> float:
+    numerador = lmbda*((lmbda*x)**(alpha-1))*exp(lmbda*x)
+    denominador = gamma(alpha)
+    return numerador/denominador
+
+
+def GammaR(pseudo: list, lmbda: float, alpha: float) -> list:
+    M = max([gamma(x) for x in range(start=.01, stop=1.01, step=.005)])
+
+
+def UniformeR(pseudo: list, a: float, b: float) -> list:
+    aceptados = []
+    M = 1/(b-a)
+    for U in pseudo:
+        while True:
+            V = np.random.uniform(0, 1)
+            T = a+((b-a)*V)
+            if(M*U <= 1/(b-a)):
+                aceptados.append(T)
+                break
+    return aceptados
+
+
 def generador_numpy(n):
     numbers = []
     for i in range(n):
@@ -41,15 +70,16 @@ def generador_numpy(n):
     return numbers
 
 
-mu = 0
-var = 1
+#mu = 1
+#var = 1
 distribucion_generada = generador_numpy(10000)
-distribucion_generada = normal(distribucion_generada, mu, var)
+#distribucion_generada = NormalR(distribucion_generada, mu, var)
 #distribucion_generada = Hipergeometricas(N=170, p=0.45, m=25, n=1000)
 #distribucion_generada = Normal(3, 0.8, 95, 10000)
-plt.hist(distribucion_generada, bins=20)
-plt.axvline(x=mu, color='b', label='axvline - full height')
+#distribucion_generada = UniformeR(distribucion_generada, 5, 15)
+"""plt.hist(distribucion_generada)
+#plt.axvline(x=mu, color='b', label='axvline - full height')
 plt.title('Histograma de una variable')
 plt.xlabel('Valor de la variable')
 plt.ylabel('Conteo')
-plt.show()
+plt.show()"""
