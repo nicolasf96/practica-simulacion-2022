a231beeb7b9c95f9a02478b7b4fd3fc034d47bf7
Ivan <ivanbisc12@gmail.com>
2022-05-21 12:16:38 -0300

    CAmbios hipergeom

--- GenDistribuido/MRechazo.py
+++ GenDistribuido/MRechazo.py
@@ -4,7 +4,7 @@ from math import sqrt, pi, exp
 
 import matplotlib.pyplot as plt
 import numpy as np
-from MTransformadas import Uniforme, Exponencial
+from MTransformadas import Uniforme, Exponencial, Hipergeometricas, Normal
 
 
 def densidad_norm(m, v, x):
@@ -18,12 +18,13 @@ def max_densidad_norm(mu_: float, var_: float) -> float:
     return max(f_0, f_1)
 
 
-def normal(pseudo: list, mu: float, var: float, a: float, b: float) -> list:
+def normal(pseudo: list, mu: float, var: float) -> list:
+    a, b = mu-(2*var), mu+(2*var)
     M = 1/(var*sqrt(2*pi))  # maximo valor de densidad de la funcion objetivo
     aceptados = []
     for U in pseudo:
         while True:
-            V = np.random.uniform(a, b)
+            V = np.random.uniform(0, 1)
             T = a+((b-a)*V)
             if(M*U <= densidad_norm(mu, var, T)):
                 # T se acepta
@@ -40,12 +41,15 @@ def generador_numpy(n):
     return numbers
 
 
-mu = 1
+"""mu = -1
 var = 1.2
-distribucion_generada = generador_numpy(8000)
+distribucion_generada = generador_numpy(10000)
 distribucion_generada = normal(
-    distribucion_generada, mu, var, mu-(2*var), mu+(2*var))
-plt.hist(distribucion_generada)
+    distribucion_generada, mu, var)"""
+distribucion_generada = Hipergeometricas(N=170, p=0.45, m=25, n=1000)
+#distribucion_generada = Normal(3, 0.8, 95, 10000)
+plt.hist(distribucion_generada, bins=45)
+#plt.axvline(x=mu, color='b', label='axvline - full height')
 plt.title('Histograma de una variable')
 plt.xlabel('Valor de la variable')
 plt.ylabel('Conteo')
--- GenDistribuido/MTransformadas.py
+++ GenDistribuido/MTransformadas.py
@@ -1,3 +1,5 @@
+from copy import error
+from decimal import DivisionByZero
 import math
 from typing import Any
 from math import log
@@ -6,9 +8,7 @@ from numpy import std
 from math import sqrt, pi, exp
 
 
-
-
-def Uniforme( a: float, b: float, n:int) -> list:
+def Uniforme(a: float, b: float, n: int) -> list:
     numbers = []
     for i in range(n):
         r = np.random.uniform(0, 1)
@@ -16,7 +16,8 @@ def Uniforme( a: float, b: float, n:int) -> list:
         numbers.append(x)
     return numbers
 
-def Exponencial(l: float, n:int) -> Any:
+
+def Exponencial(l: float, n: int) -> Any:
     numbers = []
     for i in range(n):
         ex = 1 / l
@@ -25,16 +26,18 @@ def Exponencial(l: float, n:int) -> Any:
         numbers.append(x)
     return numbers
 
-def Normal( mu: float, var: float, K: int, n:int) -> list:
+
+def Normal(mu: float, var: float, K: int, n: int) -> list:
     numbers = []
     for i in range(n):
         suma = 0
         for i in range(K):
-            r =np.random.uniform(0, 1)
+            r = np.random.uniform(0, 1)
             suma = suma + r
         x = var * (suma - K / 2) / sqrt(K / 12) + mu
         numbers.append(x)
     return numbers
+
 
 def Gamma(k: int, alpha: float, n) -> list:
     numbers = []
@@ -47,7 +50,8 @@ def Gamma(k: int, alpha: float, n) -> list:
         numbers.append(x)
     return numbers
 
-def Binomial( N: int, p: float, n: int) -> list:
+
+def Binomial(N: int, p: float, n: int) -> list:
     numbers = []
     for i in range(n):
         x = 0
@@ -58,10 +62,15 @@ def Binomial( N: int, p: float, n: int) -> list:
         numbers.append(x)
     return numbers
 
-def Hipergeometricas( N:int, p: float, m: int, n: int) -> list:
+
+def Hipergeometricas(N: int, p: float, m: int, n: int) -> list:
     numbers = []
+    n_r = N
+    p_r = p
     for i in range(n):
         x = 0.0
+        N = n_r
+        p = p_r
         for i in range(m):
             r = np.random.uniform(0, 1)
             if (r - p) <= 0:
@@ -69,12 +78,16 @@ def Hipergeometricas( N:int, p: float, m: int, n: int) -> list:
                 x += 1.0
             else:
                 s = 0.0
-            p = (N * p - s) / (N - 1.0)
+            try:
+                p = (N * p - s) / (N - 1.0)
+            except ZeroDivisionError:
+                continue
             N -= 1.0
         numbers.append(x)
     return numbers
 
-def Poisson( l: int, n: int) ->list:
+
+def Poisson(l: int, n: int) -> list:
     numbers = []
     for i in range(n):
         x = 0.0
@@ -89,7 +102,9 @@ def Poisson( l: int, n: int) ->list:
                 x += 1.0
         numbers.append(x)
     return numbers
-def Pascal(k: int, p: float, n: int)-> list:
+
+
+def Pascal(k: int, p: float, n: int) -> list:
     q = 1 - p
     numbers = []
     for i in range(n):
@@ -102,11 +117,12 @@ def Pascal(k: int, p: float, n: int)-> list:
         numbers.append(x)
     return numbers
 
-print(Uniforme(a=0, b=1, n=1000))
+
+"""print(Uniforme(a=0, b=1, n=1000))
 print(Exponencial(l=3, n=1000))
 print(Normal(mu=8, var=0.25, K=95, n=1000))
 print(Gamma(k=8, alpha=0.95, n=1000))
 print(Binomial(N=35, p=0.50, n=1000))
-#print(Hipergeometricas(N=170, p=0.45, m=25, n=1000))
+print(Hipergeometricas(N=170, p=0.45, m=25, n=1000))
 print(Poisson(l=6, n=1000))
-print(Pascal(k=4,p=0.45, n=1000))
+print(Pascal(k=4, p=0.45, n=1000))"""
