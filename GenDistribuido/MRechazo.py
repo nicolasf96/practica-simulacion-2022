b6f09e242047387180fe9bbc9f0c42cc422dec00
Ivan <ivanbisc12@gmail.com>
2022-05-23 18:55:11 -0300

    exp, poisson, binom
    hipergeom no funciona

--- GenDistribuido/MRechazo.py
+++ GenDistribuido/MRechazo.py
@@ -1,7 +1,7 @@
 from tracemalloc import start, stop
 import numpy as np
 
-from math import sqrt, pi, exp, gamma
+from math import sqrt, pi, exp, gamma, factorial, trunc
 
 
 import matplotlib.pyplot as plt
@@ -11,13 +11,6 @@ from MTransformada import UniformeT, ExponencialT, NormalT
 
 def densidad_norm(m, v, x):
     return (1/v*sqrt(2*pi))*exp(-(((x-m)**2)/2*(v**2)))
-
-
-def max_densidad_norm(mu_: float, var_: float) -> float:
-    """Devuelve el maximo valor que puede tener f de densidad en [0,1]"""
-    f_0 = densidad_norm(mu_, var_, 0)
-    f_1 = densidad_norm(mu_, var_, 1)
-    return max(f_0, f_1)
 
 
 def NormalR(pseudo: list, mu: float, des: float) -> list:
@@ -55,12 +48,18 @@ def GammaR(pseudo: list, lmbda: float, alpha: float, max_x: float) -> list:
     for U in pseudo:
         while True:
             V = np.random.uniform(0, 1)
-            T = b*V
+            T = b*V  # a=0 siempre
 
             if(M*U <= densidad_gamma(lmbda=lmbda, alpha=alpha, x=T)):
                 gmma.append(T)
                 break
     return gmma
+
+
+def ExponenecialR(pseudo: list, lmbda: float, max_x: float) -> list:
+    """Una distribucion exponencial es un caso especial de
+    distribucion Gamma donde el parametro de forma alfa=1"""
+    return GammaR(pseudo=pseudo, lmbda=lmbda, alpha=1, max_x=max_x)
 
 
 def UniformeR(pseudo: list, a: float, b: float) -> list:
@@ -74,6 +73,69 @@ def UniformeR(pseudo: list, a: float, b: float) -> list:
                 aceptados.append(T)
                 break
     return aceptados
+
+
+def densidad_Poisson(lmbda: float, x: int) -> float:
+    numerador = (lmbda**x)*exp(-lmbda)
+    denominador = factorial(x)
+    return numerador/denominador
+
+
+def PoissonR(pseudo: list, lmbda: float, max_x: int) -> list:
+    M = densidad_Poisson(lmbda=lmbda, x=trunc(lmbda))
+    poisson = []
+    for U in pseudo:
+        while True:
+            V = np.random.uniform(0, 1)
+            T = trunc(V*max_x)  # a=0 siempre, b=max_x
+            if(M*U <= densidad_Poisson(lmbda, T)):
+                poisson.append(T)
+                break
+    return poisson
+
+
+"""def densidad_Hipergeometrica(N_p: int, N_q: int, n: int, x: int) -> float:
+    N = N_p+N_q
+    a = factorial(N_p)/(factorial(x)*factorial(N_p-x))
+    b = factorial(N_q)/(factorial(N-x)*factorial(N_q-(N-x)))
+    c = factorial(N)/(factorial(n)*factorial(N-n))
+    return (a*b)/c
+
+
+def HipergeometricaR(pseudo: list, N_p: int, n: int) -> list:   #NO FUNCIONA
+    N = len(pseudo)
+    N_q = N-N_p
+    q = trunc((n*N_q)/N)
+    M = densidad_Hipergeometrica(N_p, N_q, n, q)
+    hiper = []
+    for U in pseudo:
+        while True:
+            V = np.random.uniform(0, 1)
+            T = trunc(V*N_p)
+            if(M*U <= densidad_Hipergeometrica(N_p, N_q, n, T)):
+                hiper.append(T)
+                break
+    return hiper"""
+
+
+def densidad_binomial(n: int, p: float, x: int) -> float:
+    a = factorial(n)/(factorial(x)*(factorial(n-x)))
+    b = p**x
+    c = (1-p)**(n-x)
+    return a*b*c
+
+
+def BinomialR(pseudo: list, n: int, p: float) -> list:
+    M = densidad_binomial(n, p, trunc(n*p))
+    binom = []
+    for U in pseudo:
+        while True:
+            V = np.random.uniform(0, 1)
+            T = trunc(n*V)
+            if(M*U <= densidad_binomial(n, p, T)):
+                binom.append(T)
+                break
+    return binom
 
 
 def generador_numpy(n):
@@ -92,7 +154,11 @@ distribucion_generada = generador_numpy(10000)
 #distribucion_generada = UniformeR(distribucion_generada, 5, 15)
 #distribucion_generada = NormalT(distribucion_generada, 2, .5)
 #distribucion_generada = GammaR(distribucion_generada, lmbda=2, alpha=2, max_x=20)
-plt.hist(distribucion_generada, bins=35)
+#plt.hist(distribucion_generada, bins=35)
+#distribucion_generada = PoissonR(distribucion_generada, 10, 25)
+#distribucion_generada = BinomialR(distribucion_generada, 40, .5)
+#distribucion_generada = HipergeometricaR(distribucion_generada, 900, 160)
+#plt.hist(distribucion_generada, bins=round(sqrt(len(distribucion_generada))),  edgecolor='black')
 #plt.axvline(x=mu, color='b', label='axvline - full height')
 plt.title('Histograma de una variable')
 plt.xlabel('Valor de la variable')
